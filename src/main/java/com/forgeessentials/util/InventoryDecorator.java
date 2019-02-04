26bee234b781aa18e8e88511032347a7c061a587
Ivan Sennov <3aklinatel@gmail.com>
2019-02-05 00:46:29 +0800

    Bring InventoryIterator into InventoryManipulator as internal class. InventoryManipulator now implements a Decorator pattern.

---
+++ src/main/java/com/forgeessentials/util/InventoryDecorator.java
@@ -0,0 +1,78 @@
+package com.forgeessentials.util;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
+
+public abstract class InventoryDecorator implements IInventory {
+    protected IInventory inventory;
+
+    public InventoryDecorator(IInventory inventory) {
+        this.inventory = inventory;
+    }
+
+    @Override
+    public int getSizeInventory() {
+        return inventory.getSizeInventory();
+    }
+
+    @Override
+    public ItemStack getStackInSlot(int slot) {
+        return inventory.getStackInSlot(slot);
+    }
+
+    @Override
+    public ItemStack decrStackSize(int slot, int amount) {
+        return inventory.decrStackSize(slot, amount);
+    }
+
+    @Override
+    public ItemStack getStackInSlotOnClosing(int slot) {
+        return inventory.getStackInSlotOnClosing(slot);
+    }
+
+    @Override
+    public void setInventorySlotContents(int slot, ItemStack itemStack) {
+        inventory.setInventorySlotContents(slot, itemStack);
+    }
+
+    @Override
+    public String getInventoryName() {
+        return inventory.getInventoryName();
+    }
+
+    @Override
+    public boolean hasCustomInventoryName() {
+        return inventory.hasCustomInventoryName();
+    }
+
+    @Override
+    public int getInventoryStackLimit() {
+        return inventory.getInventoryStackLimit();
+    }
+
+    @Override
+    public void markDirty() {
+        inventory.markDirty();
+    }
+
+    @Override
+    public boolean isUseableByPlayer(EntityPlayer entityPlayer) {
+        return inventory.isUseableByPlayer(entityPlayer);
+    }
+
+    @Override
+    public void openInventory() {
+        inventory.openInventory();
+    }
+
+    @Override
+    public void closeInventory() {
+        inventory.closeInventory();
+    }
+
+    @Override
+    public boolean isItemValidForSlot(int slot, ItemStack itemStack) {
+        return inventory.isItemValidForSlot(slot, itemStack);
+    }
+}
--- src/main/java/com/forgeessentials/util/InventoryManipulator.java
+++ src/main/java/com/forgeessentials/util/InventoryManipulator.java
@@ -1,30 +1,215 @@
 package com.forgeessentials.util;
+
 import com.sun.istack.internal.NotNull;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 
-import java.util.function.Predicate;
+import java.util.*;
+import java.util.function.Consumer;
 
-public class InventoryManipulator implements Iterable<ItemStack> {
-    private IInventory inventory;
-    private InventoryIterator iterator;
-    private Predicate<ItemStack> iteratorFilter;
-
-    public InventoryManipulator(@NotNull IInventory inventory) {
-        this.inventory = inventory;
-    }
-
-    public void setIteratorFilter(Predicate<ItemStack> filter) {
-        iteratorFilter = filter;
+public class InventoryManipulator extends InventoryDecorator implements Iterable<ItemStack>
+{
+    public InventoryManipulator(IInventory inventory) {
+        super(inventory);
     }
 
     /**
      * @inheritDocs
      */
     @Override
-    public InventoryIterator iterator() {
-        return iteratorFilter == null
-                ? new InventoryIterator(inventory)
-                : new InventoryIterator(inventory, iteratorFilter);
+    public Iterator<ItemStack> iterator() {
+        return new Itr();
     }
+
+    public int getFreeSlots()
+    {
+        int total = inventory.getSizeInventory();
+
+        for (ItemStack stack : this)
+        {
+            if (stack != null)
+                total -= 1;
+        }
+
+        return total;
+    }
+
+    public int getFreeSpaceForItem(ItemStack targetStack)
+    {
+        int stackLimit = getStackLimit(targetStack);
+        int total = inventory.getSizeInventory() * stackLimit;
+
+        for (ItemStack stack : this)
+        {
+            if (stack != null)
+            {
+                total -= stack.isItemEqual(targetStack)
+                        ? stack.stackSize
+                        : stackLimit;
+            }
+        }
+
+        return total;
+    }
+
+    public boolean isFit(ItemStack stack)
+    {
+        return getFreeSpaceForItem(stack) >= stack.stackSize;
+    }
+
+    public boolean pushItemStack(ItemStack pushStack)
+    {
+        if (!isFit(pushStack))
+            return false;
+
+        int stackLimit = getStackLimit(pushStack);
+
+        int i = 0;
+        for (ItemStack stack : this)
+        {
+            if (stack == null)
+            {
+                inventory.setInventorySlotContents(i, pushStack);
+                break;
+            }
+            else
+            {
+                if (stack.isItemEqual(pushStack))
+                {
+                    int amount = Math.min(stackLimit - stack.stackSize, pushStack.stackSize);
+                    inventory.setInventorySlotContents(i, pushStack.splitStack(amount));
+                }
+            }
+            ++i;
+        }
+
+        return true;
+    }
+
+    public int countItems(@NotNull ItemStack searchItemStack)
+    {
+        int count = 0;
+
+        for (ItemStack slotStack : this)
+        {
+            if (slotStack != null && slotStack.isItemEqual(searchItemStack))
+            {
+                count += slotStack.stackSize;
+            }
+        }
+
+        return count;
+    }
+
+    public int getStackLimit(@NotNull ItemStack stack)
+    {
+        return Math.min(inventory.getInventoryStackLimit(), stack.getMaxStackSize());
+    }
+
+    /**
+     * An iterator over inventory.
+     * Helper class for more convenient iterating over inventory slots.
+     *
+     * @see Iterator
+     * @see IInventory
+     */
+    protected class Itr implements Iterator<ItemStack>
+    {
+        private int index = -1;
+
+        /**
+         * @inheritDoc
+         */
+        @Override
+        public boolean hasNext()
+        {
+            return index != inventory.getSizeInventory();
+        }
+
+        /**
+         * {@inheritDoc}
+         * If filter is present
+         */
+        @Override
+        public ItemStack next()
+        {
+            try
+            {
+                return inventory.getStackInSlot(++index);
+            }
+            catch (ArrayIndexOutOfBoundsException e)
+            {
+                throw new NoSuchElementException();
+            }
+        }
+
+        /**
+         * @inheritDoc
+         */
+        @Override
+        public void remove()
+        {
+            requireIndexLegal();
+            inventory.setInventorySlotContents(index, null);
+        }
+
+        /**
+         * {@inheritDoc}
+         * <p>This implementation also checks for a possible null returned from {@code next}</p>
+         */
+        @Override
+        public void forEachRemaining(Consumer<? super ItemStack> action) {
+            Objects.requireNonNull(action);
+            while (hasNext())
+            {
+                ItemStack stack = next();
+
+                if (stack != null) {
+                    action.accept(next());
+                }
+            }
+        }
+
+        /**
+         * Return inventory slot index on current iteration
+         *
+         * @return Current index
+         */
+        public int getIndex()
+        {
+            return index;
+        }
+
+        /**
+         * Replaces the last element returned by {@link #next} with the specified
+         * element (optional operation).
+         * This call can be made only if {@link #remove} have not been called
+         * after the last call to {@code next}.
+         *
+         * @param itemStack the item stack with which to replace the last element
+         *                  returned by {@code next}
+         *
+         * @throws IllegalStateException if {@code next} have not been called, or
+         *         {@code remove} have been called after the last call to {@code next}
+         */
+        public void set(ItemStack itemStack)
+        {
+            requireIndexLegal();
+            inventory.setInventorySlotContents(index, itemStack);
+        }
+
+        /**
+         * Requires {@code next} have been called or {@code remove} have not been
+         * called after last call to {@code next}.
+         *
+         * @throws IllegalStateException if {@code next} have not been called, or
+         *         {@code remove} have been called after the last call to {@code next}
+         */
+        private void requireIndexLegal()
+        {
+            if (index == -1)
+                throw new IllegalStateException();
+        }
+    }
+
 }
